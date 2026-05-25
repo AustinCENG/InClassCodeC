8adc04f0633d10c7d95dbbc9473514ccfe28e17a
AustinCENG <tianyuxin@gmail.com>
2026-05-25 16:36:26 -0400

    edited the calculation

--- Week2/BasicCalculationOrder.c
+++ Week2/BasicCalculationOrder.c
@@ -3,6 +3,16 @@
 #include <stdio.h>
 int main()
 {
+    int a = 5, b = 2;
+    float c = 1.0*a/b;  // what woudl be c? should be 2.0.
+    float d = ((float)a)/b; // similar to the conversion in c. should be 2.0 here. 
+    float e = a/b;  // what woudl be e? should be 2.0
+    e = 5/2; // what woudl be e? should be 2.0
+    printf("The result of 5/2 is: %d, and 5%%2 is : %d\n", a/b, a%b);
+    printf("The result of a/b is: %f", c);
+
+  
+  
     int i = 2;
     int k = 30;
     int u = 100;
