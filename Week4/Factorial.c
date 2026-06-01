38768c8e554e8844f42b92d07df1f9c6f74b4636
AustinCENG <tianyuxin@gmail.com>
2026-06-01 16:40:23 -0400

    updating

--- Week4/Factorial.c
+++ Week4/Factorial.c
@@ -11,11 +11,18 @@ int main()
     n = 5;  // for user input. 
 
     /* Computes the product n x (n-1) x (n-2) x . . . x 2 x 1 */
+    // i is the counter here. 
     for (i = n; i > 1; --i) {
          product = product * i;
     }
-    printf("%d! = %d", n, product);
-    /* Returns function result */
+    
+    // What is the value of i at this moment? After exiting the loop
+    // it should be the first number that breaks the Loop condition. 
+    printf("After the loop, i is %d\n", i);
+    
+
+    printf("%d! = %d\n", n, product);
+
     return (0);
 }
 
