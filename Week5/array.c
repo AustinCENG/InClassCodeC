0eb277ec7680904815432a1448ca694139b728f2
AustinCENG <tianyuxin@gmail.com>
2026-06-10 16:59:32 -0400

    updates

---
+++ Week5/array.c
@@ -0,0 +1,18 @@
+void main(){
+    
+ int primeUnder100[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 
+                       31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
+                       73, 79, 83, 89, 97}; // all 25 prime numbers under 100 
+
+ char vowels[] = "This is a long string"; // define a string in C 
+
+ float realNumberswithError[];  // this is not allowed. If no initialization like line 12, 
+                           // you have to have the size of the array. 
+ float realNumbers[] = {1.2, 2.4, 3.6, 9.8}; // This array has 4 elements. 
+
+ int fillinZeros[100] = {1, 2, 3, 4}; // This array has 100 elements.
+                                      // The first 4 elements are initialized. 
+                                      // All other elements will be 0 
+
+ int ZeroArrays[1000] = {0}; // all elements in the array will be initialized to 0.
+}
