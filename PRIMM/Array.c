820e8ffbf3f85582a05621892c9d23d18ff75c44
AustinCENG <tianyuxin@gmail.com>
2026-06-17 15:24:04 -0400

    updated

--- PRIMM/Array.c
+++ PRIMM/Array.c
@@ -1,12 +1,26 @@
 #include <stdio.h> 
- 
+
+// print out N elements from array
+void printAllElements(int *array, int N)  // if N is 10. 
+{
+    for (int i=N-1; i>=0; i--)
+    {
+      printf("The %d th element of the array is: %d\n", i, array[i]);
+      // or printf("The %d th element of the array is: %d\n", i, *(array+i));
+
+    }
+}
+
+
 int main() { 
+    int a[2];  // no initialization, then a[i] will be always random. 
+
     int numbers[10] = {10, 20, 30, 40}; 
     int testInit[100] = {0};  // typical way to initialize an array with all 0 elements. 
     int *ptr = numbers; // Pointing to the beginning of the array numbers[0]
                         // same as  int *ptr = &numbers[0];
 
-    printf("Value 1: %d\n", *ptr); // dereference the ptr to its value. 
+                        printf("Value 1: %d\n", *ptr); // dereference the ptr to its value. 
     printf("Value 2: %d\n", *(ptr + 2)); // this is NOT *ptr + 2;
                                          //  it is actually same as numbers[2]
 
@@ -15,5 +29,7 @@ int main() {
     ptr++; // increment ptr to ptr+1, i.e., &numbers[1]
     
     printf("Value 3: %d\n", *ptr); 
+
+    printAllElements(numbers, 10); // numbers is a pointer. 
     return 0; 
 } 
--- PRIMM/Struct.c
+++ PRIMM/Struct.c
@@ -1,27 +1,42 @@
 #include <stdio.h> 
 #include <string.h> 
- 
+
 struct Student { 
-    char name[50]; 
+    char name[50];  // members. 
     int id; 
     double gpa; 
     int credits_earned; 
     char major[30]; 
 }; 
+struct Book{
+    char title[100];
+    char author[50];
+    int year;
+    double price; 
+};
  
+
 // Function to update student record via pointer 
 void updateStudentRecord(struct Student *s, double newGpa, int newCredits) { 
-    s->gpa = newGpa; 
+    s->gpa = newGpa; // not s.gpa. 
     s->credits_earned += newCredits; 
 } 
+
+void updatePrice(struct Book *bookptr, float discount){
+// do some caclulation and deference the struct pointer
+}
  
 int main() { 
     struct Student s1 = {"Alice Smith", 101, 3.5, 60, "Electrical Engineering"}; 
- 
+    struct Student s2[10]; // s2 is an array with 10 elements. each element is a struct Student 
+    
+    struct Book bookexample = {"C programming", "King", 1998, 34.9};
+
     printf("Original GPA: %.2f, Credits: %d\n", s1.gpa, s1.credits_earned); 
  
     // Passing the address of the struct 
-    updateStudentRecord(&s1, 3.8, 15); 
+    updateStudentRecord(&s1, 3.8, 15); // s1 is a regular variable, it is not an array. 
+                                       // so you need to use &s1 to find the address to pass. 
  
     printf("Updated GPA: %.2f, Credits: %d\n", s1.gpa, s1.credits_earned); 
  
